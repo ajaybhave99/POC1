2ae1ab44ae2c247a81a53e2f237fbcc61a072abd
BasicProj <ajaybhave99@gmail.com>
2025-12-28 19:03:46 +0530

    Changes done

--- POC1/Program.cs
+++ POC1/Program.cs
@@ -23,12 +23,8 @@ builder.Services.AddScoped<IArtRepository, ArtRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
-
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
